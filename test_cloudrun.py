246301e1f1a0882b2046fa4f0a6cd4ba236d381d
Oscar <oscar.medrano2002@gmail.com>
2026-04-26 00:39:01 +0200

    add kubernetes

--- test_cloudrun.py
+++ test_cloudrun.py
@@ -5,7 +5,7 @@ path = r"H:\Documents\ING3 IA\UE  Entreprises\Big data\MLOPS\Projet\mlops_cours-
 dataset = pd.read_csv(path)
 dataset = dataset.drop(["user_session", "user_id", "purchased"], axis=1)
 
-URL = "https://purchase-predict-api-511275009279.europe-west1.run.app/predict"
+URL = "http://34.120.173.161/predict"
 
 response = requests.post(URL, json=dataset.sample(n=5).to_json())
 print("Status:", response.status_code)
